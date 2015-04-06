8c3908df83e0abd5e42f1254c419d5fcf58e3546
Dawa Sherpa <dsherpa@mymail.mines.edu>
2015-04-05 22:46:41 -0600

    Added some admin and user auth

--- unit12/config/routes.rb
+++ unit12/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   resources :selected_pets
 
   resources :carts
--- unit12/db/schema.rb
+++ unit12/db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326073651) do
+ActiveRecord::Schema.define(version: 20150406044300) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150326073651) do
     t.string   "description"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.integer  "pet_type",    default: 3
+    t.integer  "pet_type",    default: 2
     t.string   "PET_STATUS"
     t.string   "picture"
   end
@@ -38,5 +56,23 @@ ActiveRecord::Schema.define(version: 20150326073651) do
 
   add_index "selected_pets", ["cart_id"], name: "index_selected_pets_on_cart_id"
   add_index "selected_pets", ["pet_id"], name: "index_selected_pets_on_pet_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
